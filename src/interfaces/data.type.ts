e2a6e2e665456b23d65fd0cbed15455dbd917cf5
bmancilla23 <bmancilla119@gmail.com>
2024-05-23 14:13:17 -0500

    feat: data created

---
+++ src/interfaces/data.type.ts
@@ -0,0 +1,12 @@
+// Definir la interfaz para el producto
+export interface Product {
+  img: string;
+  title: string;
+  rating: number;
+  reviews: string;
+  prevPrice: string;
+  newPrice: string;
+  company: string;
+  color: string;
+  category: string;
+}

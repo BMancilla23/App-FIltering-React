629ffdcf17a4b47975dc7ec816a3b38f7ffaba4b
bmancilla23 <bmancilla119@gmail.com>
2024-05-23 14:12:43 -0500

    feat: recommended created

---
+++ src/components/recommended/Recommended.tsx
@@ -0,0 +1,14 @@
+export const Recommended = () => {
+  return (
+    <section className=''>
+      <h2 className='text-2xl ml-5 m-5'>Recomendado</h2>
+      <div className='flex ml-5'>
+        <button className='btns'>Todos los productos</button>
+        <button className='btns'>Nike</button>
+        <button className='btns'>Adidas</button>
+        <button className='btns'>Puma</button>
+        <button className='btns'>Vans</button>
+      </div>
+    </section>
+  );
+};
